b8fdac7bf55cfc0a594f9062c277c6d33942146b
EnzoQuinalha <enzoquinalha31@gmail.com>
2025-02-11 08:56:44 -0300

    s

--- model/database.py
+++ model/database.py
@@ -1,1 +1,24 @@
-import mysql
+import mysql.connector as mc# Biblioteca do conector do MySQL
+from mysql.connector import Error # Importanto a classe Error para tratar as mensagens de erro do código
+from dotenv import load_dotenv # Importando a função load_dotenv 
+from os import getenv # Importando a função getenv
+
+class Database:
+    def __init__(self):
+        load_dotenv()
+        self.host = getenv('DB_HOST')
+        self.username = getenv('DB_USER')
+        self.password = getenv('DB_PSWD')
+        self.database = getenv('DB_NAME')
+        self.connection = None # Inicialização da conexão
+        self.cursor = None # Inicialização do cursor
+
+    def conectar(self):
+        """Estabelece uma conexão com o banco de dados."""
+        try: 
+            self.connection = mysql.connector.connect(
+                host = self.host,
+                database = self.database,
+                password = self.password  
+
+            )
